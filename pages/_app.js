1d629798849dd408c204e16add4aa7cbd53876f9
Alex <alex.beciana@gmail.com>
2021-09-04 21:15:31 -0400

    Imported fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/bsh6twa.css"/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
