57811cf48119260b813d08fd130f3f81880a9e3b
Alex <alex.beciana@gmail.com>
2021-09-11 21:45:06 -0400

    Began rendering of contact form

---
+++ components/utils/forms/ContactForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ContactForm extends React.Component {
+
+    render() {
+        
+        return (
+            <React.Fragment>
+                <form>
+
+                </form>
+            </React.Fragment>
+        )
+    }
+}
+
+export default ContactForm
