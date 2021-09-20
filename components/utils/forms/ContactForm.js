d313d3bec1b6f6de76b105330d5763c2a0827fa4
Alex <alex.beciana@gmail.com>
2021-09-20 00:14:43 -0400

    Completed rendering of contact form and added to homepage

--- components/utils/forms/ContactForm.js
+++ components/utils/forms/ContactForm.js
@@ -2,13 +2,93 @@ import React from 'react';
 
 class ContactForm extends React.Component {
 
+    state = {
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    }
+
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+
+    }
+
     render() {
         
         return (
             <React.Fragment>
-                <form>
-
-                </form>
+                <section
+                    className="flex"
+                >
+                    <section
+                        className="mx-32 mt-10 bg-black rounded-3xl w-1/2"
+                    >
+                        <h2 className="text-white text-5xl pt-5 pl-10">say hi!</h2>
+                        <form
+                            className="pt-5 pb-10 px-10"
+                            onSubmit={this.submitHandler}
+                        >
+                            <div>
+                                <input
+                                    name="name"
+                                    type="text"
+                                    placeholder="Name*"
+                                    required
+                                    value={this.state.name}
+                                    className="w-full py-2 px-4 rounded-3xl focus:outline-none mt-1"
+                                    onChange={this.changeHandler}
+                                />
+                            </div>
+                            <br />
+                            <div>
+                                <input
+                                    name="email"
+                                    type="email"
+                                    placeholder="Email*"
+                                    required
+                                    value={this.state.email}
+                                    className="w-full py-2 px-4 rounded-3xl focus:outline-none mt-1"
+                                    onChange={this.changeHandler}
+                                />
+                            </div>
+                            <br/>
+                            <div>
+                                <input
+                                    name="subject"
+                                    type="text"
+                                    placeholder="Subject*"
+                                    required
+                                    value={this.state.subject}
+                                    className="w-full py-2 px-4 rounded-3xl focus:outline-none mt-1"
+                                    onChange={this.changeHandler}
+                                />
+                            </div>
+                            <br/>
+                            <div>
+                                <textarea
+                                    name="message"
+                                    placeholder="Message*"
+                                    required
+                                    value={this.state.message}
+                                    className="w-full py-2 px-4 rounded-2xl focus:outline-none mt-1"
+                                    onChange={this.changeHandler}
+                                />
+                            </div>
+                            <br />
+                            <input
+                                type="submit"
+                                className="bg-yellow text-black text-lg py-1 px-2 rounded-xl cursor-pointer"
+                            />
+                        </form>
+                    </section>
+                </section>
             </React.Fragment>
         )
     }
