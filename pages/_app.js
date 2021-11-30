f6e537b2afe5ee2823d2135279fd3545009093ef
Alex <alex.beciana@gmail.com>
2021-11-29 22:32:44 -0500

    Updated _document and app

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,17 @@ import '../styles/reviews.css'
 
 import 'tailwindcss/tailwind.css'
 
+import Head from 'next/head'
+
 import NavBar from '../components/utils/navbar'
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      </Head>
       <NavBar/>
       <Component {...pageProps} />
     </>
