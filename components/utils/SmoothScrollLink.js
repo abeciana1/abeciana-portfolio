a53df26fb554fbe1f44730b56641d8bfcd9aa953
Alex <alex.beciana@gmail.com>
2021-12-28 22:48:38 -0500

    Added smooth scroll link comp

---
+++ components/utils/SmoothScrollLink.js
@@ -0,0 +1,14 @@
+import cx from 'classnames'
+import { Link, animateScroll as scroll } from "react-scroll";
+
+
+const SmoothScrollLink = (props) => {
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default SmoothScrollLink
