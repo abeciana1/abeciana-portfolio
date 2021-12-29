28436a208f7b737b8438cdbf61cd73f77ed6dde4
Alex <alex.beciana@gmail.com>
2021-12-28 22:43:38 -0500

    Started work on nav bar update

---
+++ components/utils/NavBar.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link'
+import PageMargin from './PageMargin'
+import cx from 'classnames'
+import { Squash as Hamburger } from 'hamburger-react'
+import { withRouter } from 'next/router'
+
+
+const NavBar = () => {
+
+
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    )
+}
+
+export default NavBar
--- components/utils/PageNavBar.js
+++ components/utils/PageNavBar.js
@@ -2,9 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link'
 import PageMargin from './PageMargin'
 import cx from 'classnames'
-
 import { Squash as Hamburger } from 'hamburger-react'
-import { withRouter } from 'next/router'
 
 const PageNavBar = () => {
 
