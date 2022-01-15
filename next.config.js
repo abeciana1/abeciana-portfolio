e0a315d3d7f09b7af58a85cc3535685675065bd1
Alex <alex.beciana@gmail.com>
2022-01-15 13:06:06 -0500

    Fixed build issue && removed comment && removed console log

--- next.config.js
+++ next.config.js
@@ -9,10 +9,10 @@ module.exports = {
     async redirects() {
         return [
             {
-                source: 'about',
-                destination: '/',
-                permanent: true
-            }
+            "source": '/about',
+            "destination": '/',
+            "permanent": true,
+            },
         ]
-    }
+    },
 }
